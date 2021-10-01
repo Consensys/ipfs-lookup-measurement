08d0e86854ec4ab7ffa3663a16d3e3202bc153d2
James Fan <james.fan@consensys.net>
2021-10-01 15:57:37 +1000

    run experiment on parallel for simple nodes

--- controller/pkg/simplenode/simplenode.go
+++ controller/pkg/simplenode/simplenode.go
@@ -5,31 +5,42 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 )
 
 // Experiment publish message from one node, and lookup from all other nodes
 func Experiment(ctx context.Context, nodesList []string) {
-	for i, node := range nodesList {
-		msg := fmt.Sprintf("node=%d,time=%v,key=1", i, time.Now())
-		cid, err := PutString(ctx, node, msg)
-		if err != nil {
-			log.Println(node, err)
-			continue
-		}
-		log.Println("PutString", node, msg)
+	var wg sync.WaitGroup
 
-		for j, lookupNode := range nodesList {
-			if i == j {
-				continue
-			}
-			startAt := time.Now()
-			_, err = LookupString(ctx, lookupNode, cid)
+	for i, node := range nodesList {
+		wg.Add(1)
+
+		go func(i int, node string) {
+			defer wg.Done()
+
+			msg := fmt.Sprintf("node=%d,time=%v,key=1", i, time.Now())
+			cid, err := PutString(ctx, node, msg)
 			if err != nil {
-				log.Println(lookupNode, err)
-				continue
+				log.Println(node, err)
+				return
 			}
-			log.Println("LookupString", lookupNode, time.Since(startAt))
-		}
+			log.Println("PutString", node, msg)
+
+			for j, lookupNode := range nodesList {
+				if i == j {
+					continue
+				}
+				startAt := time.Now()
+				_, err = LookupString(ctx, lookupNode, cid)
+				if err != nil {
+					log.Println(lookupNode, err)
+					continue
+				}
+				log.Printf("LookupString put=%v lookup=%v elapsed=%v i=%d j=%d\n", node, lookupNode, time.Since(startAt), i, j)
+			}
+		}(i, node)
 	}
+
+	wg.Wait()
 }
