605f8302b3dba5f7e9b011a10ec259e5034f8d56
wcgcyx <wcgcyx@gmail.com>
2021-10-05 14:16:38 +1000

    Update docker-entrypoint.sh

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -8,4 +8,5 @@ echo "      host: $HOST_NAME" >> promtail-local-config.yaml
 ./promtail-linux-amd64 -config.file=promtail-local-config.yaml &
 # Start grafana
 ./go-ipfs/cmd/ipfs/ipfs init
+./go-ipfs/cmd/ipfs/ipfs config --json Discovery.MDNS '{"Enabled": false}'
 ./go-ipfs/cmd/ipfs/ipfs daemon > /app/all.log 2>&1
