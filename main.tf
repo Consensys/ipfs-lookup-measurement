fa12a0b98a683f029aad574ca8fce6d2a70c278d
wcgcyx <wcgcyx@gmail.com>
2021-10-11 01:05:20 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,3 +77,47 @@ resource "aws_security_group" "security_ipfs_testing_monitor" {
     Name = "security_ipfs_testing_monitor"
   }
 }
+
+resource "aws_security_group" "security_ipfs_testing_node" {
+  name        = "security_ipfs_testing_node"
+  description = "security group for ipfs testing node"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4001
+    to_port     = 4001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 4001
+    to_port     = 4001
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9010
+    to_port     = 9010
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security_ipfs_testing_node"
+  }
+}
