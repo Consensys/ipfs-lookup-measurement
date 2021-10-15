4cf5b6de461026cc24372ca3d9f157845b7f03aa
wcgcyx <wcgcyx@gmail.com>
2021-10-15 12:55:22 +1000

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 key=$(cat .key)
-terraform apply -auto-approve -var="KEY=$key"
+terraform apply -var="KEY=$key"
 terraform output > nodes-list.out
 monitorIP=$(head -1 ./nodes-list.out | cut -d'"' -f2)
 echo "monitor URL is $monitorIP:3000"
